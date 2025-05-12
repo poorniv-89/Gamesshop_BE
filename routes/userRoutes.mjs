11a2052c5b77f2ca9d3b27b539b3b6c26efe8f64
poorniv-89 <poorniv.89@gmail.com>
2025-05-12 10:05:39 -0600

    login post route updated

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -38,6 +38,30 @@ router.post('/register', async (req, res) => {
     })
 
 
+
+})
+
+router.post('/login', async(req,res)=>{
+    const {email, password } = req.body;
+
+    if(!email || !password)
+        return res.status(400).json({
+    message: 'Email and password are required'})
+    let user = await User.findOne({email})
+    if(!user)
+        return res.status(400).json({
+    message: 'Invalid credentials'})
+    if(user.password !== password)
+    {
+        return res.status(400).json({
+            message: 'invalid credentials'
+        })
+    }
+    res.status(200).json({
+        message: "Login successful",
+        "userid": user._id,
+        "cart_id": user.cart})
+
 })
 
 export default router;
